6f1d3df818b95c04252b53351881a8824bad66b5
K-Othman <karim_othman88@hotmail.com>
2023-10-15 23:36:03 +0100

    building the 3 features

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@
 import { Menu } from "@headlessui/react";
 import React from "react";
 import Landing from "./components/landing/Page";
+import CategoriesPage from "./components/categories/Page";
 
 export default function Home() {
   return (
     <main>
       <Landing />
+      <CategoriesPage />
     </main>
   );
 }
